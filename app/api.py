9b46046696caced54348220a35d248e349787595
Nidhish Puthiyadath <nidhish.nambiar@gmail.com>
2025-09-15 13:31:49 -0700

    Add Flask API, requirements, and README for ticket assignee POC

---
+++ app/api.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+import joblib
+import os
+
+app = Flask(__name__)
+
+MODEL_PATH = os.path.join(os.path.dirname(__file__), '../model/ticket_assigner.pkl')
+model = joblib.load(MODEL_PATH)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    title = data.get('title', '')
+    description = data.get('description', '')
+    labels = data.get('labels', [])
+    # If labels is a list, join as space-separated string
+    if isinstance(labels, list):
+        labels_text = ' '.join(labels)
+    else:
+        labels_text = str(labels)
+    text = f'{title} {description} {labels_text}'
+    assignee = model.predict([text])[0]
+    return jsonify({'assignee': assignee})
+
+if __name__ == '__main__':
+    app.run(debug=True)
